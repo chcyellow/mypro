b05a930ad8dcb6ed15c1c2290f39091eb796deef
chcyellow <chcplus@gmail.com>
2016-06-03 13:15:47 +0800

    Second Commit Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello{
 	public static void main (String args[]){
-		System.out.println("Hello chcyellow");
-		System.out.println("chcplus@gmail.com")
+		System.out.println("世界，你好!")
 		//Add a Line.
 
 	}
